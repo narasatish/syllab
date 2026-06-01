a2932fe56767d552680b22144432a293d184fc6c
Nara <narasatish966@gmail.com>
2026-06-01 18:19:54 +0530

    chore: bump SW cache to v45 for deploy

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
  *
  * Bump CACHE_VERSION on every deploy to force clients to pick up the new build.
  */
-const CACHE_VERSION = 'syllab-v44-2026-06-01-state-syllabus-fallback';
+const CACHE_VERSION = 'syllab-v45-2026-06-01-deploy-colleges-blogs';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
