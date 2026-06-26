c06fd96c08a1599063c6268d80a0418e47acd390
Nara <narasatish966@gmail.com>
2026-06-26 18:44:03 +0530

    a11y: darken .eyebrow text to clear WCAG 4.5:1 (light + dark)

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
  *
  * Bump CACHE_VERSION on every deploy to force clients to pick up the new build.
  */
-const CACHE_VERSION = 'syllab-v176-2026-06-26-trust-band-a11y-snapsolve';
+const CACHE_VERSION = 'syllab-v177-2026-06-26-eyebrow-contrast';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
