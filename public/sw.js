04262f73a22455947d654fdf52a6a9475411fd0f
Nara <narasatish966@gmail.com>
2026-06-30 22:55:47 +0530

    expand(story): ~372 lessons taught both ways (124/193 files); coverage 534/534, images preserved; SW v199

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
  *
  * Bump CACHE_VERSION on every deploy to force clients to pick up the new build.
  */
-const CACHE_VERSION = 'syllab-v198-2026-06-30-expand-234';
+const CACHE_VERSION = 'syllab-v199-2026-06-30-expand-372';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
