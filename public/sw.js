56bacfebd55c9943161783ac75b852f64bf5ccd5
Nara <narasatish966@gmail.com>
2026-07-06 21:55:20 +0530

    feat(home): move LandingPrep band lower (below Latest Updates, above FAQ); SW v216

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
  *
  * Bump CACHE_VERSION on every deploy to force clients to pick up the new build.
  */
-const CACHE_VERSION = 'syllab-v215-2026-07-05-hindi-landingprep';
+const CACHE_VERSION = 'syllab-v216-2026-07-05-landingprep-lower';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
