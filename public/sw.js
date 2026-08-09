c025b0d41b6ee12b09ba850a0ef6b7f2f8723f49
Nara <narasatish966@gmail.com>
2026-08-09 09:59:52 +0530

    chore: bump CACHE_VERSION to v302 for the exam-watch / quiz / tab-bar deploy

--- public/sw.js
+++ public/sw.js
@@ -9,7 +9,7 @@
  *
  * Bump CACHE_VERSION on every deploy to force clients to pick up the new build.
  */
-const CACHE_VERSION = 'syllab-v301-2026-08-08-defer-sdks-past-lcp';
+const CACHE_VERSION = 'syllab-v302-2026-08-09-exam-watch-quiz-tabbar';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
